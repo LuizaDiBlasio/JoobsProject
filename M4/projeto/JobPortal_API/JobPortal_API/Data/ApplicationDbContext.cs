214835eafe3fc32531be35fa4b19334292318b6c
LuizaDiBlasio <luizabandeira90@gmail.com>
2026-05-07 17:30:42 +0100

    SeedDb e UserHelper

--- M4/projeto/JobPortal_API/JobPortal_API/Data/ApplicationDbContext.cs
+++ M4/projeto/JobPortal_API/JobPortal_API/Data/ApplicationDbContext.cs
@@ -16,52 +16,52 @@ namespace JobPortal_API.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<IdentityRole>().HasData(
-                new IdentityRole
-                {
-                    Id = "1", // cuidado: Identity usa string como ID
-                    Name = "Admin",
-                    NormalizedName = "ADMIN"
-                },
-                new IdentityRole
-                {
-                    Id = "2",
-                    Name = "Candidato",
-                    NormalizedName = "CANDIDATO"
-                },
-                new IdentityRole
-                {
-                    Id = "3",
-                    Name = "Empresa",
-                    NormalizedName = "EMPRESA"
-                }
-            );
+            //modelBuilder.Entity<IdentityRole>().HasData(
+            //    new IdentityRole
+            //    {
+            //        Id = "1", // cuidado: Identity usa string como ID
+            //        Name = "Admin",
+            //        NormalizedName = "ADMIN"
+            //    },
+            //    new IdentityRole
+            //    {
+            //        Id = "2",
+            //        Name = "Candidato",
+            //        NormalizedName = "CANDIDATO"
+            //    },
+            //    new IdentityRole
+            //    {
+            //        Id = "3",
+            //        Name = "Empresa",
+            //        NormalizedName = "EMPRESA"
+            //    }
+            //);
 
-            // Criar o user admin
-            var hasher = new PasswordHasher<ApplicationUser>();
+            //// Criar o user admin
+            //var hasher = new PasswordHasher<ApplicationUser>();
 
-            var admin = new ApplicationUser
-            {
-                Id = "10", 
-                UserName = "admin@admin.com",
-                NormalizedUserName = "ADMIN@ADMIN.COM",
-                Email = "admin@admin.com",
-                NormalizedEmail = "ADMIN@ADMIN.COM",
-                EmailConfirmed = true,
-                SecurityStamp = Guid.NewGuid().ToString("D")
-            };
+            //var admin = new ApplicationUser
+            //{
+            //    Id = "10", 
+            //    UserName = "admin@admin.com",
+            //    NormalizedUserName = "ADMIN@ADMIN.COM",
+            //    Email = "admin@admin.com",
+            //    NormalizedEmail = "ADMIN@ADMIN.COM",
+            //    EmailConfirmed = true,
+            //    SecurityStamp = Guid.NewGuid().ToString("D")
+            //};
 
-            // Setar a senha
-            admin.PasswordHash = hasher.HashPassword(admin, "Admin123!"); 
+            //// Setar a senha
+            //admin.PasswordHash = hasher.HashPassword(admin, "Admin123!"); 
 
-            modelBuilder.Entity<ApplicationUser>().HasData(admin);
+            //modelBuilder.Entity<ApplicationUser>().HasData(admin);
 
-            // Dar a role de Admin pro user
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-            {
-                RoleId = "1", // o id da role Admin
-                UserId = "10" // o id do user admin
-            });
+            //// Dar a role de Admin pro user
+            //modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            //{
+            //    RoleId = "1", // o id da role Admin
+            //    UserId = "10" // o id do user admin
+            //});
 
 
             modelBuilder.Entity<Review>()
---
+++ M4/projeto/JobPortal_API/JobPortal_API/Utilities/Interfaces/IUserHelper.cs
@@ -0,0 +1,52 @@
+﻿using JobPortal_API.DTOs;
+using JobPortal_API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortal_API.Utilities.Interfaces
+{
+    public interface IUserHelper
+    {
+        Task<ApplicationUser> CreateUser([FromBody] RegisterationRequestDTO model);
+
+
+        Task<IdentityResult> AddUserAsync(ApplicationUser user, string password);
+
+
+        Task AddUserToRoleAsync(ApplicationUser user, string roleName);
+
+        Task<IdentityResult> ChangePasswordAsync(ApplicationUser user, string oldPassword, string newPassword);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task CreateRolesAsync();
+
+        Task<ApplicationUser> GetUserByEmailAsync(string email);
+
+        Task<bool> IsUserInRoleAsync(ApplicationUser user, string roleName);
+
+        Task<Microsoft.AspNetCore.Identity.SignInResult> LoginAsync(RegisterationRequestDTO model);
+
+        Task LogoutAsync();
+
+        Task<IdentityResult> UpdateUserAsync(ApplicationUser user);
+
+        Task<string> GenerateEmailConfirmationTokenAsync(ApplicationUser user);
+
+        Task<string> GeneratePasswordResetTokenAsync(ApplicationUser user);
+
+        Task<IdentityResult> ConfirmEmailAsync(ApplicationUser user, string token);
+
+        Task<ApplicationUser> GetUserByIdAsync(string id);
+
+        Task<IdentityResult> ResetPasswordAsync(ApplicationUser user, string token, string password);
+
+        Task<Microsoft.AspNetCore.Identity.SignInResult> ValidatePasswordAsync(ApplicationUser user, string password);
+
+        Task<IList<string>> GetRolesAsync(ApplicationUser user);
+
+
+        Task<bool> ExistsAsync(string email);
+    }
+}
