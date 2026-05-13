1fa85914300bb155db43d25e1df149373469c2b2
erika <erika@LAPTOP-ERIKA13>
2026-05-13 01:57:38 +0100

    validacoes PERFIL empresa
    PERFIL empresa:
     - Implementadas validações no Model de Empresa (campos obrigatórios e
       data de nascimento).
     - Refinada a validação da data de nascimento impedindo valores
       inválidos.
     - Feedback de validação direto no formulário para campos
       obrigatórios.Please enter the commit message for your changes. Lines starting

--- M4/projeto/Cliente_APIRemoto/teste cliente/Models/Empresa.cs
+++ M4/projeto/Cliente_APIRemoto/teste cliente/Models/Empresa.cs
@@ -6,13 +6,27 @@ namespace teste_cliente.Models
     {
         [Key]
         public int IdEmpresa { get; set; }
+
         [Required]
         public string Nome { get; set; }
-        public string? Localidade { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "A localidade é obrigatória.")]
+        public string? Localidade { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        [RegularExpression(@"^[0-9]{9}$", ErrorMessage = "O telefone deve conter 9 números.")]
         public int? Telefone { get; set; }
+
+        [Required(ErrorMessage = "O número de funcionários é obrigatório.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Valor inválido.")]
         public int? NoFuncionarios { get; set; }
+
+        [Required(ErrorMessage = "A zona de atuação é obrigatória.")]
         public string? ZonaAtuacao { get; set; }
+
         public string? LinkedIn { get; set; }
         public string? Facebook { get; set; }
 
