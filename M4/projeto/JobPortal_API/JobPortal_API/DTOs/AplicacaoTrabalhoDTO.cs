6428128aa13eaeb601e0c7471bb7c3f2b2fe5759
erika <erika@LAPTOP-ERIKA13>
2026-05-15 14:59:12 +0100

    style: oculta id da aplicacao na interface do swagger
    Problema: O IdAplicacao aparecia no corpo do POST como campo editável.
    
    Solução: Aplicado [JsonIgnore] no DTO para garantir que o ID seja gerado apenas pelo banco.

--- M4/projeto/JobPortal_API/JobPortal_API/DTOs/AplicacaoTrabalhoDTO.cs
+++ M4/projeto/JobPortal_API/JobPortal_API/DTOs/AplicacaoTrabalhoDTO.cs
@@ -1,14 +1,14 @@
-﻿namespace JobPortal_API.DTOs
+﻿using System.Text.Json.Serialization;
+
+namespace JobPortal_API.DTOs
 {
     public class AplicacaoTrabalhoDTO
     {
+        [JsonIgnore] // Isso faz o campo sumir do Swagger e do JSON de entrada
         public int IdAplicacao { get; set; }
-
-        public int IdOferta { get; set; }
-      
+        public int IdOferta { get; set; }      
         public int IdCandidato { get; set; }
         public DateTime DataAplicacao { get; set; } = DateTime.Now;
-
         public string? aplicacaoAceite { get; set; } = null;
     }
 }
