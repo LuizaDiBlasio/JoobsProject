5171b22e1fb51736ac6ae2a6557827046daad94c
Luís <maialuis87@gmail.com>
2026-05-26 12:39:07 +0100

    .

--- M4/projeto/JobPortal_API/JobPortal_API/Models/Notifications.cs
+++ M4/projeto/JobPortal_API/JobPortal_API/Models/Notifications.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using JobPortal_API.Models;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JobPortal_API.Models
@@ -8,10 +9,15 @@ namespace JobPortal_API.Models
         public int IdNotification { get; set; }
         [Key]
         public string UserId { get; set; }
-        [ForeignKey("UserId")]
-
         public string? Notification { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsRead { get; set; }
+ 
+        [ForeignKey("IdCandidato")]
+        public Candidato? Candidato { get; set; }
+        public int IdEmpresa { get; set; }
+
+        [ForeignKey("IdEmpresa")]
+        public Empresa? Empresa { get; set; }
     }
 }
